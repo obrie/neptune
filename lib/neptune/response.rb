c5f8b446742ade05a5bde405704ca6cb7e0311db
Aaron Pfeifer <aaron.pfeifer@tapjoy.com>
2015-07-14 21:19:00 -0400

    Remove invalid attribute

--- lib/neptune/response.rb
+++ lib/neptune/response.rb
@@ -3,9 +3,6 @@ require 'neptune/resource'
 module Neptune
   class Response < Resource
     # @return [Fixnum]
-    attribute :size, Int32
-
-    # @return [Fixnum]
     attribute :correlation_id, Int32
   end
 end
